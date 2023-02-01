80cba9ea21e7989e90c0278e45fc5dde6b30d8c5
marcin <brand.marcin@gmail.com>
2023-02-01 22:40:45 +0000

    zadanie43672

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+function abc(n) {
+    if (n < 100) {
+        console.log('Liczba nie znajduje się w przedziale');
+
+    } else if (n > 200) {
+        console.log('Liczba nie znajduje się w przedziale');
+
+    } else { console.log('Liczba znajduje się w przedziale'); }
+
+}
+
+console.log(abc(99));
